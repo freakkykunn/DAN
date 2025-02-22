f00938dcd952d92adaa7bc9a85128f79b8aad723
freakkykunn <154701834+freakkykunn@users.noreply.github.com>
2025-02-22 10:03:47 +0700

    Add files via upload

---
+++ src/Than/main.java
@@ -0,0 +1,8 @@
+package Than;
+
+public class main {
+    public static void main(String[] args) {
+        dacn frame = new dacn();
+        frame.setVisible(true);
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author havan
+ *
+ */
+module Than {
+}
